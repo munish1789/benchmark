19e7b4b079e823d4a25205afde3d9f08bc664ec1
Andrey Koltsov <a99920641@gmail.com>
2021-12-14 12:08:05 +0300

    Add measurements of transaction phases.

--- driver-pravega/src/main/java/io/openmessaging/benchmark/driver/pravega/PravegaBenchmarkTransactionProducer.java
+++ driver-pravega/src/main/java/io/openmessaging/benchmark/driver/pravega/PravegaBenchmarkTransactionProducer.java
@@ -31,7 +31,7 @@ import com.google.errorprone.annotations.concurrent.GuardedBy;
 
 import java.nio.ByteBuffer;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 import java.util.UUID;
 
 public class PravegaBenchmarkTransactionProducer implements BenchmarkProducer {
@@ -47,12 +47,77 @@ public class PravegaBenchmarkTransactionProducer implements BenchmarkProducer {
     private final int eventsPerTransaction;
     private int eventCount = 0;
     private ByteBuffer timestampAndPayload;
+    // -- Additional measurements
+    private long noneToOpenStartEpoch;
+    private long noneToOpenEndEpoch;
+
+    private ExecutorService executorService;
+
+    /**
+     * TODO: We must create thread pool with N tasks, N = committing transactions.
+     * Thread pool must check statuses of them in non-blocking fashion.
+     * We might have a transaction queue. We'd go through the queue and check the status, remove from the queue COMMITTED transactions afterwards.
+     */
+    public static class PollingJob implements Callable<Void> {
+        private Transaction<ByteBuffer> transaction;
+        private final long noneToOpenStartEpoch;
+        private final long noneToOpenEndEpoch;
+        private final long commitFinishedEpoch;
+        private final long commitProcessStartEpoch;
+
+        public PollingJob(final long noneToOpenStartEpoch, final long noneToOpenEndEpoch, final long commitProcessStartEpoch, final long commitFinishedEpoch, Transaction transaction) {
+            this.noneToOpenStartEpoch = noneToOpenStartEpoch;
+            this.noneToOpenEndEpoch = noneToOpenEndEpoch;
+            this.commitProcessStartEpoch = commitProcessStartEpoch;
+            this.commitFinishedEpoch = commitFinishedEpoch;
+            this.transaction = transaction;
+        }
+
+        @Override
+        // TODO: Scheduled feature? If not committed - return
+        public Void call() throws Exception {
+
+            final long committedFinishedEpoch = getTimeStatusReached(this.transaction, Transaction.Status.COMMITTED);
+            // NONE <-> OPEN
+            final long durationNoneToOpenMs = (this.noneToOpenEndEpoch - this.noneToOpenStartEpoch) / (long) 1000000;
+            // OPEN <-> COMMITTING (including writes)
+            final long durationOpenToCommittingMs = (this.commitFinishedEpoch - this.noneToOpenEndEpoch) / (long) 1000000;
+            // COMMITTING <-> COMMITTED
+            final long durationCommittingToCommittedMs = (committedFinishedEpoch - this.commitFinishedEpoch) / (long) 1000000;
+            // COMMIT SPECIFIC
+            final long commitProcessOnly = (commitFinishedEpoch - commitProcessStartEpoch) / (long) 1000000;
+            // WRITES ONLY
+            final long writeOnlyDurationMs = (commitProcessStartEpoch - this.noneToOpenEndEpoch) / (long) 1000000;
+            PravegaBenchmarkTransactionProducer.log.debug("Transaction---" + transaction.getTxnId() + "---OPEN---" + durationNoneToOpenMs +
+                    "---COMMITTING---" + durationOpenToCommittingMs + "---COMMITTED---" +
+                    durationCommittingToCommittedMs + "---EPOCH---" + System.currentTimeMillis() +
+                    "---COMMITONLY---" + commitProcessOnly + "---WRITEONLY---" + writeOnlyDurationMs);
+            return null;
+        }
+
+        /**
+         * Ensures @param transaction reached given @param status
+         * @return system time when given status had been reached.
+         */
+        private long getTimeStatusReached(Transaction transaction, Transaction.Status status) {
+            while(transaction.checkStatus() != status) {
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return 0;
+                }
+            }
+            return System.nanoTime();
+        }
+    }
 
     public PravegaBenchmarkTransactionProducer(String streamName, EventStreamClientFactory clientFactory,
-            boolean includeTimestampInEvent, boolean enableConnectionPooling, int eventsPerTransaction) {
+                                               boolean includeTimestampInEvent, boolean enableConnectionPooling, int eventsPerTransaction) {
         log.info("PravegaBenchmarkProducer: BEGIN: streamName={}", streamName);
 
         final String writerId = UUID.randomUUID().toString();
+        this.executorService = Executors.newFixedThreadPool(15);
         transactionWriter = clientFactory.createTransactionalEventWriter(writerId, streamName,
                 new ByteBufferSerializer(),
                 EventWriterConfig.builder().enableConnectionPooling(enableConnectionPooling).build());
@@ -64,13 +129,10 @@ public class PravegaBenchmarkTransactionProducer implements BenchmarkProducer {
     public CompletableFuture<Void> sendAsync(Optional<String> key, byte[] payload) {
         try {
             if (transaction == null) {
+                this.noneToOpenStartEpoch = System.nanoTime();
                 transaction = transactionWriter.beginTxn();
-            }
-            if (this.probeRequested(key)) { // Populate probe transaction with the sufficient amount of events.
-                while (eventCount < this.eventsPerTransaction) {
-                    transaction.writeEvent(key.get(), ByteBuffer.wrap(payload));
-                    eventCount++;
-                }
+                // beginTxn() is Synchronous => do not wait for status OPEN implicitly
+                this.noneToOpenEndEpoch = System.nanoTime();
             }
             if (includeTimestampInEvent) {
                 if (timestampAndPayload == null || timestampAndPayload.limit() != Long.BYTES + payload.length) {
@@ -85,7 +147,18 @@ public class PravegaBenchmarkTransactionProducer implements BenchmarkProducer {
             }
             if (++eventCount >= eventsPerTransaction) {
                 eventCount = 0;
+                final long commitProcessStartEpoch = System.nanoTime();
                 transaction.commit();
+                final long commitFinishedEpoch = System.nanoTime();
+                // BegintTxn(), commit(), write()
+                final long beginCommitDurMs = (this.noneToOpenEndEpoch - this.noneToOpenStartEpoch) / (long) 1000000;
+                final long writeExclusiveDurMs = (commitProcessStartEpoch - this.noneToOpenEndEpoch) / (long) 1000000;
+                final long commitExclusiveDurMs = (commitFinishedEpoch - commitProcessStartEpoch) / (long) 1000000;
+                log.info("---BEGINTXN---" + beginCommitDurMs +
+                        "---WRITE---" + writeExclusiveDurMs + "---COMMITT---" +
+                        commitExclusiveDurMs + "---EPOCH---" + System.currentTimeMillis());
+                // this.executorService.submit(new PollingJob(this.noneToOpenStartEpoch, this.noneToOpenEndEpoch, commitProcessStartEpoch, commitFinishedEpoch, this.transaction));
+
                 transaction = null;
             }
         } catch (TxnFailedException e) {
@@ -105,6 +178,8 @@ public class PravegaBenchmarkTransactionProducer implements BenchmarkProducer {
 
     @Override
     public void close() throws Exception {
+        this.executorService.shutdown();
+        executorService.awaitTermination(30, TimeUnit.MILLISECONDS);
         synchronized (this) {
             if (transaction != null) {
                 transaction.abort();
@@ -114,13 +189,4 @@ public class PravegaBenchmarkTransactionProducer implements BenchmarkProducer {
         transactionWriter.close();
     }
 
-    /** Indicates if producer probe had been requested by OpenMessaging benchmark.
-     * @param key - key provided to the probe.
-     * @return true in case requested event had been created in context of producer probe.
-     */
-    private final boolean probeRequested(Optional<String> key) {
-        // For the expected key, see: LocalWorker.probeProducers()
-        final String expectedKey = "key";
-        return key.isPresent() && key.get().equals(expectedKey);
-    }
 }
